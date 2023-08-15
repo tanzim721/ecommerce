cca585a55c29f1526c6cc50f8745da695ba26f3b
tanzim721 <tanzimulislam799@gmail.com>
2023-08-15 19:39:11 +0600

    15-08-23
    ok2

--- ecommerces/app/Http/Controllers/AdminController.php
+++ ecommerces/app/Http/Controllers/AdminController.php
@@ -22,12 +22,12 @@ class AdminController extends Controller
         $admin_password = md5($request->password);
         $result=Admin::where('admin_email', $admin_email)->where('admin_password', $admin_password)->first();
         if($result){
-            // Session::put('admin_id', $result->admin_id);
-            // Session::put('admin_name', $result->admin_name);
+            Session::put('admin_id', $result->admin_id);
+            Session::put('admin_name', $result->admin_name);
             return Redirect::to('/dashboard')->with('success','You are Logged in sucessfully.');
         }
         else{
-            // Session::put('message', 'Email or Password Invalid');
+            Session::put('message', 'Email or Password Invalid');
             return Redirect::to('/admins')->with('success','You are Logged in error.');
         }
     }
