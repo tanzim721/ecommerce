84626a58613e49495099842a4899727272d681ac
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-08-08 23:16:02 +0600

    frontend design

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -21,4 +22,5 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::get('admin/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+Route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin'])->name('admin.dashboard');
+
