47067142e0b17617611350b6c61cd7d618e8b130
tanzim721 <53827851+tanzim721@users.noreply.github.com>
2024-08-09 00:13:10 +0600

    admin dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Admin</h2>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <input type="submit" value="Logout">
-    </form>
-</body>
+<html>
+  <head>
+    @include('admin.head')
+  </head>
+  <body>
+
+    @include('admin.header')
+
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+      @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            <h2 class="h5 no-margin-bottom">Dashboard</h2>
+          </div>
+        </div>
+        @include('admin.content')
+
+        @include('admin.footer')
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('backend/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('backend/js/graindashboard.js') }}"> </script>
+    <script src="{{ asset('backend/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('backend/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('backend/js/charts-home.js') }}"></script>
+    <script src="{{ asset('backend/js/front.js') }}"></script>
+  </body>
 </html>
